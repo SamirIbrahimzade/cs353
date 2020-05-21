d178e3565e794d9d6b30c0bbc71073487b723c22
Samir Ibrahimzade <samir.ibrahimzade@ug.bilkent.edu.tr>
2020-05-21 20:51:42 +0300

    -createTrack wo review -inviteDeveloper

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, flash, redirect, url_for, session, log
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+import sys
+
 
 app = Flask(__name__)
 app.debug = True
@@ -23,87 +25,103 @@ mysql = MySQL(app)
 def index():
     return render_template("index.html")
 
-
+# Dev Functions
 @app.route("/devSignUp.html", methods=['GET', 'POST'])
 def devSignUp():
+    if session.get("logged_in") == True:
+        logout()
+    else:
+        class RegisterForm(Form):
+            name = StringField('Name', [validators.Length(min=1, max=50)])
+            username = StringField('Username', [validators.Length(min=4, max=25)])
+            email = StringField('Email', [validators.Length(min=6, max=50)])
+            password = PasswordField('Password', [validators.DataRequired(),
+                        validators.EqualTo('confirm', message="Passwords do not match!")])
+            confirm = PasswordField('Confirm Password')
 
-    class RegisterForm(Form):
-        name = StringField('Name', [validators.Length(min=1, max=50)])
-        username = StringField('Username', [validators.Length(min=4, max=25)])
-        email = StringField('Email', [validators.Length(min=6, max=50)])
-        password = PasswordField('Password', [validators.DataRequired(),
-                    validators.EqualTo('confirm', message="Passwords do not match!")])
-        confirm = PasswordField('Confirm Password')
+        form = RegisterForm(request.form)
+        print("here")
+        if request.method == 'POST':
+            name = form.name.data
+            email = form.email.data
+            password = form.password.data
 
-    form = RegisterForm(request.form)
-    print("here")
-    if request.method == 'POST':
-        name = form.name.data
-        email = form.email.data
-        password = form.password.data
+            # Create cursor
+            cur = mysql.connection.cursor()
 
-        # Create cursor
-        cur = mysql.connection.cursor()
-        
-        # Create new User
-        cur.execute("INSERT INTO User(name, email, password) VALUES(%s, %s, %s)", (name, email, password))
-        mysql.connection.commit()
+            # Create new User and check if already exists
+            try:
+                cur.execute("INSERT INTO User(name, email, password) VALUES(%s, %s, %s)", (name, email, password))
+                mysql.connection.commit()
+            except:
+                app.logger.info('Email exists already')
+                
+                return redirect(url_for("devSignUp"))
 
-        # Get User Id
-        cur.execute("Select user_id from User where email='{0}'".format(email));
-        mysql.connection.commit();
-        userid = cur.fetchall()[0]['user_id']
+            # Get User Id
+            cur.execute("Select user_id from User where email='{0}'".format(email));
+            mysql.connection.commit();
+            userid = cur.fetchall()[0]['user_id']
 
-        print (userid)
+            print (userid)
 
-        # Create new Developer 
-        cur.execute("INSERT INTO Developer (developer_id) VALUES(%s)", [userid])
-        mysql.connection.commit()
+            # Create new Developer
+            cur.execute("INSERT INTO Developer (developer_id) VALUES(%s)", [userid])
+            mysql.connection.commit()
 
-        # Close connection
-        cur.close()
-
-        flash("You are now registered successfully", 'success')
-
-        print("here22")
-        return redirect(url_for('index'))
+            # Close connection
+            cur.close()
+            flash("You are now registered successfully", 'success')
+            return redirect(url_for('index'))
     return render_template("devSignUp.html", form = form)
 
 @app.route("/devSignIn.html", methods=['GET', 'POST'])
 def devSignIn():
-
-    class SignIn(Form):
-
-        email = StringField('Email', [validators.Length(min=6, max=50)])
-        password = PasswordField('Password', [validators.DataRequired(),
-                    validators.EqualTo('confirm', message="Passwords do not match!")])
-    
-    form = SignIn(request.form)
-
-    if (request.method == "POST"):
-
-        email = form.email.data
-        password = form.password.data
+    if session.get("logged_in") == True:
+        return redirect(url_for("devHome"))
+    elif request.method == 'POST':
+        email = request.form['email']
+        password_candidate = request.form['password']
 
         # Create Cursor
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM User, Developer " + 
-         "WHERE User.user_id = Developer.developer_id AND User.email = '{0}' AND User.password = '{1}'".format(email , password))
-        mysql.connection.commit()
 
-        # Get response
-        queryResponse = cur.fetchall();
+        result = cur.execute("SELECT * FROM User WHERE email = %s", [email])
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
 
-        if (len(queryResponse) == 0):
-            flash("Email or Password is incorrect")
+            # Compare the passwords
+            if password == password_candidate:
+                app.logger.info('PASSWORD MATCHED')
+                session['logged_in'] = True
+                session['email'] = email
+                flash("You are now logged in", "success")
+                return redirect(url_for('devHome'))
+            else:
+                return redirect(url_for('devHome'))
+                app.logger.info('PASSWORD NOT MATCHED')
+                error = 'INVALID LOGIN'
+                return render_template("devSignIn.html", error = error)
+
+            # Close cursor
+            cur.close()
         else:
-            return redirect(url_for("devHome"))
-    
+            error = 'USER NOT FOUND'
+            app.logger.info('USER NOT FOUND')
+            return render_template("devSignIn.html", error = error)
+    return render_template("devSignIn.html")
 
-    return render_template("devSignIn.html" , form=form)
+@app.route("/logout.html")
+def logout():
+    session.clear()
+    flash("You are logged out!", "success")
+    return redirect(url_for("index"))
 
+compCreatedTrackId = 0;
 
-@app.route("/compCreateTrack.html")
+@app.route("/compCreateTrack.html", methods=['GET', 'POST'])
 def compCreateTrack():
 
     # Create cursor
@@ -117,25 +135,49 @@ def compCreateTrack():
 
     if len(queryResponse) == 0:
         flash ("There is not any question")
-    else :
-        print (queryResponse)
+    #else :
+     #   print (queryResponse)
+
+    #print(len(queryResponse), file=sys.stderr)
+    
+    
+    #trackResult[0];
 
     class CreateTrackForm(Form):
-        id = StringField('Id', [validators.Length(min=1, max=50)])
-    
+        id = StringField('id', [validators.Length(min=1, max=50)])
     form = CreateTrackForm(request.form)
-    
 
-   # id = form.id.data
-   #  print(id)
-    if (request.method == "POST"):
+    global compCreatedTrackId 
+
+    if request.method == "POST":
         id = form.id.data
-        print(53454)
+        print(id, file=sys.stderr)
+        print(compCreatedTrackId)
+        print("IDIDIDIDIDID===",id)
 
+        if id !='0':
+            cur.execute("INSERT INTO trackquestions (question_id, track_id) VALUES(%s,%s)", [id,compCreatedTrackId]);      
+            mysql.connection.commit()   
+        else:
+            return redirect(url_for('compReviewTrack')) 
+    else:
+        trackName = "Track";
+        cur.execute("INSERT INTO track (track_name) VALUES(%s)", [trackName]);      
+        mysql.connection.commit() 
+        
+        cur.execute("SELECT track_id FROM track");      
+        mysql.connection.commit() 
+        trackResult = cur.fetchall();
+        
+        compCreatedTrackId = (trackResult[len(trackResult)-1]["track_id"]);
+        print (compCreatedTrackId);
 
+        #print (buttons[0], buttons[1],buttons[2],buttons[3])
+        #return redirect(url_for('compHome'))
+       
     return render_template("compCreateTrack.html", question = queryResponse)
 
-@app.route("/compInviteDeveloper.html")
+@app.route("/compInviteDeveloper.html",methods=['GET', 'POST'])
 def compInviteDeveloper():
 
 
@@ -143,23 +185,36 @@ def compInviteDeveloper():
     cur = mysql.connection.cursor()
 
     # Create new User
-    cur.execute("SELECT * FROM Developer")      
+    cur.execute("SELECT name FROM user, developer WHERE user_id = developer_id")      
     mysql.connection.commit()
-
     queryResponse = cur.fetchall();
 
+    print(queryResponse)
+
     if len(queryResponse) == 0:
-        flash ("There is not any question")
+        flash ("There is not any developer")
     else :
         print (queryResponse)
 
+    
+    class InviteForm(Form):
+        id = StringField('id', [validators.Length(min=1, max=50)])
+    form = InviteForm(request.form)
+
+    global compCreatedTrackId 
+
+    if request.method == "POST":
+        id = form.id.data
+        print(id, file=sys.stderr)
 
 
-    return render_template("compInviteDeveloper.html")
+
+    return render_template("compInviteDeveloper.html",developer = queryResponse)
 
 @app.route("/compHome.html")
 def compHome():
     return render_template("compHome.html")
+
 
 @app.route("/compSelectTrack.html")
 def compSelectTrack():
--- dbfile.sql
+++ dbfile.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: db
 -- ------------------------------------------------------
--- Server version	5.7.30-0ubuntu0.18.04.1
+-- Server version	10.4.11-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,170 +16,197 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Answer`
+-- Table structure for table `answer`
 --
 
-DROP TABLE IF EXISTS `Answer`;
+DROP TABLE IF EXISTS `answer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Answer` (
+CREATE TABLE `answer` (
   `question_id` int(11) NOT NULL,
   `developer_id` int(11) NOT NULL,
   `score` int(11) DEFAULT NULL,
   PRIMARY KEY (`question_id`,`developer_id`),
   KEY `developer_id` (`developer_id`),
-  CONSTRAINT `Answer_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `Question` (`question_id`),
-  CONSTRAINT `Answer_ibfk_2` FOREIGN KEY (`developer_id`) REFERENCES `Developer` (`developer_id`)
+  CONSTRAINT `Answer_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`),
+  CONSTRAINT `Answer_ibfk_2` FOREIGN KEY (`developer_id`) REFERENCES `developer` (`developer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Answer`
+-- Dumping data for table `answer`
 --
 
-LOCK TABLES `Answer` WRITE;
-/*!40000 ALTER TABLE `Answer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Answer` ENABLE KEYS */;
+LOCK TABLES `answer` WRITE;
+/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Comment`
+-- Table structure for table `comment`
 --
 
-DROP TABLE IF EXISTS `Comment`;
+DROP TABLE IF EXISTS `comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Comment` (
+CREATE TABLE `comment` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
   `discussion_id` int(11) DEFAULT NULL,
-  `comment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `description` varchar(512) DEFAULT NULL,
   UNIQUE KEY `comment_id` (`comment_id`),
   KEY `discussion_id` (`discussion_id`),
-  CONSTRAINT `Comment_ibfk_1` FOREIGN KEY (`discussion_id`) REFERENCES `Discussion` (`discussion_id`)
+  CONSTRAINT `Comment_ibfk_1` FOREIGN KEY (`discussion_id`) REFERENCES `discussion` (`discussion_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Comment`
+-- Dumping data for table `comment`
 --
 
-LOCK TABLES `Comment` WRITE;
-/*!40000 ALTER TABLE `Comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Comment` ENABLE KEYS */;
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Developer`
+-- Table structure for table `comprep`
 --
 
-DROP TABLE IF EXISTS `Developer`;
+DROP TABLE IF EXISTS `comprep`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Developer` (
+CREATE TABLE `comprep` (
+  `compRep_id` int(11) NOT NULL AUTO_INCREMENT,
+  `comp_name` varchar(256) DEFAULT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`compRep_id`),
+  CONSTRAINT `compRep_ibfk_1` FOREIGN KEY (`compRep_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comprep`
+--
+
+LOCK TABLES `comprep` WRITE;
+/*!40000 ALTER TABLE `comprep` DISABLE KEYS */;
+INSERT INTO `comprep` VALUES (20,'AITools','2020-05-20 12:31:31'),(22,'Askldna','2020-05-20 12:33:02'),(23,'Bilkent','2020-05-20 13:42:25'),(24,'123','2020-05-20 13:43:29'),(26,'123','2020-05-20 18:21:27');
+/*!40000 ALTER TABLE `comprep` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `developer`
+--
+
+DROP TABLE IF EXISTS `developer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `developer` (
   `developer_id` int(11) NOT NULL AUTO_INCREMENT,
-  `regDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `regDate` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`developer_id`),
-  CONSTRAINT `Developer_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `User` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `Developer_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Developer`
+-- Dumping data for table `developer`
 --
 
-LOCK TABLES `Developer` WRITE;
-/*!40000 ALTER TABLE `Developer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Developer` ENABLE KEYS */;
+LOCK TABLES `developer` WRITE;
+/*!40000 ALTER TABLE `developer` DISABLE KEYS */;
+INSERT INTO `developer` VALUES (27,'2020-05-20 18:34:00');
+/*!40000 ALTER TABLE `developer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Discussion`
+-- Table structure for table `discussion`
 --
 
-DROP TABLE IF EXISTS `Discussion`;
+DROP TABLE IF EXISTS `discussion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Discussion` (
+CREATE TABLE `discussion` (
   `discussion_id` int(11) NOT NULL AUTO_INCREMENT,
   `question_id` int(11) DEFAULT NULL,
   UNIQUE KEY `discussion_id` (`discussion_id`),
   KEY `question_id` (`question_id`),
-  CONSTRAINT `Discussion_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `Question` (`question_id`)
+  CONSTRAINT `Discussion_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Discussion`
+-- Dumping data for table `discussion`
 --
 
-LOCK TABLES `Discussion` WRITE;
-/*!40000 ALTER TABLE `Discussion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Discussion` ENABLE KEYS */;
+LOCK TABLES `discussion` WRITE;
+/*!40000 ALTER TABLE `discussion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `discussion` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Job`
+-- Table structure for table `job`
 --
 
-DROP TABLE IF EXISTS `Job`;
+DROP TABLE IF EXISTS `job`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Job` (
+CREATE TABLE `job` (
   `developer_id` int(11) NOT NULL,
   `compRep_id` int(11) NOT NULL,
   `jobDescription` varchar(512) DEFAULT NULL,
   `endDate` date DEFAULT NULL,
   PRIMARY KEY (`developer_id`,`compRep_id`),
   KEY `compRep_id` (`compRep_id`),
-  CONSTRAINT `Job_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `Developer` (`developer_id`),
-  CONSTRAINT `Job_ibfk_2` FOREIGN KEY (`compRep_id`) REFERENCES `compRep` (`compRep_id`)
+  CONSTRAINT `Job_ibfk_1` FOREIGN KEY (`developer_id`) REFERENCES `developer` (`developer_id`),
+  CONSTRAINT `Job_ibfk_2` FOREIGN KEY (`compRep_id`) REFERENCES `comprep` (`compRep_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Job`
+-- Dumping data for table `job`
 --
 
-LOCK TABLES `Job` WRITE;
-/*!40000 ALTER TABLE `Job` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Job` ENABLE KEYS */;
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Leaderboard`
+-- Table structure for table `leaderboard`
 --
 
-DROP TABLE IF EXISTS `Leaderboard`;
+DROP TABLE IF EXISTS `leaderboard`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Leaderboard` (
+CREATE TABLE `leaderboard` (
   `leaderboard_id` int(11) NOT NULL AUTO_INCREMENT,
   `dept_name` varchar(32) DEFAULT NULL,
   `track_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`leaderboard_id`),
   KEY `track_id` (`track_id`),
-  CONSTRAINT `Leaderboard_ibfk_1` FOREIGN KEY (`track_id`) REFERENCES `Track` (`track_id`)
+  CONSTRAINT `Leaderboard_ibfk_1` FOREIGN KEY (`track_id`) REFERENCES `track` (`track_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Leaderboard`
+-- Dumping data for table `leaderboard`
 --
 
-LOCK TABLES `Leaderboard` WRITE;
-/*!40000 ALTER TABLE `Leaderboard` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Leaderboard` ENABLE KEYS */;
+LOCK TABLES `leaderboard` WRITE;
+/*!40000 ALTER TABLE `leaderboard` DISABLE KEYS */;
+/*!40000 ALTER TABLE `leaderboard` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Question`
+-- Table structure for table `question`
 --
 
-DROP TABLE IF EXISTS `Question`;
+DROP TABLE IF EXISTS `question`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Question` (
+CREATE TABLE `question` (
   `question_id` int(11) NOT NULL AUTO_INCREMENT,
   `dept_name` varchar(32) DEFAULT NULL,
   `description` varchar(512) DEFAULT NULL,
@@ -187,123 +214,121 @@ CREATE TABLE `Question` (
   `difficulty` varchar(16) DEFAULT NULL,
   `approval` int(4) DEFAULT NULL,
   PRIMARY KEY (`question_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Question`
+-- Dumping data for table `question`
 --
 
-LOCK TABLES `Question` WRITE;
-/*!40000 ALTER TABLE `Question` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Question` ENABLE KEYS */;
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'Algorithms','Very interesting question!','','low',1),(2,'Database','Very interesting question2!','','medium',1),(3,'Graphs','Very interesting question3!','','high',1),(4,'Graphs','Very interesting question4!','','high',1);
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Score`
+-- Table structure for table `score`
 --
 
-DROP TABLE IF EXISTS `Score`;
+DROP TABLE IF EXISTS `score`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Score` (
+CREATE TABLE `score` (
   `leaderboard_id` int(11) DEFAULT NULL,
   `developer_id` int(11) DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
   KEY `leaderboard_id` (`leaderboard_id`),
   KEY `developer_id` (`developer_id`),
-  CONSTRAINT `Score_ibfk_1` FOREIGN KEY (`leaderboard_id`) REFERENCES `Leaderboard` (`leaderboard_id`),
-  CONSTRAINT `Score_ibfk_2` FOREIGN KEY (`developer_id`) REFERENCES `Developer` (`developer_id`)
+  CONSTRAINT `Score_ibfk_1` FOREIGN KEY (`leaderboard_id`) REFERENCES `leaderboard` (`leaderboard_id`),
+  CONSTRAINT `Score_ibfk_2` FOREIGN KEY (`developer_id`) REFERENCES `developer` (`developer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Score`
+-- Dumping data for table `score`
 --
 
-LOCK TABLES `Score` WRITE;
-/*!40000 ALTER TABLE `Score` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Score` ENABLE KEYS */;
+LOCK TABLES `score` WRITE;
+/*!40000 ALTER TABLE `score` DISABLE KEYS */;
+/*!40000 ALTER TABLE `score` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Track`
+-- Table structure for table `track`
 --
 
-DROP TABLE IF EXISTS `Track`;
+DROP TABLE IF EXISTS `track`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Track` (
+CREATE TABLE `track` (
   `track_id` int(11) NOT NULL AUTO_INCREMENT,
   `no_questions` int(11) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `question_id` int(11) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
   `track_name` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`track_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Track`
+-- Dumping data for table `track`
 --
 
-LOCK TABLES `Track` WRITE;
-/*!40000 ALTER TABLE `Track` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Track` ENABLE KEYS */;
+LOCK TABLES `track` WRITE;
+/*!40000 ALTER TABLE `track` DISABLE KEYS */;
+INSERT INTO `track` VALUES (1,5,'2020-05-20 19:43:08','track1'),(2,4,'2020-05-20 19:52:27','track2');
+/*!40000 ALTER TABLE `track` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `User`
+-- Table structure for table `trackquestions`
 --
 
-DROP TABLE IF EXISTS `User`;
+DROP TABLE IF EXISTS `trackquestions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `User` (
+CREATE TABLE `trackquestions` (
+  `question_id` int(11) NOT NULL,
+  `track_id` int(11) NOT NULL,
+  PRIMARY KEY (`question_id`,`track_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trackquestions`
+--
+
+LOCK TABLES `trackquestions` WRITE;
+/*!40000 ALTER TABLE `trackquestions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trackquestions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) DEFAULT NULL,
-  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (20,'Askari Iqbal','ai@g.com','$5$rounds=535000$s7YCwiwEZWcjkM.R$7kQ.RSC0Siu/gwjLrwNzT6/u0nBsn0Z203ZZiU4yvz/','2020-05-20 12:16:27'),(22,'Abdullah Qutb','aasfkjasndai@g.com','$5$rounds=535000$6ToEchMwP2mgMO02$o1MrAdlEUa8rfmD/Lz7Ob4eTEX9VekHkcq.XaxHLLjA','2020-05-20 12:33:02');
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `compRep`
---
-
-DROP TABLE IF EXISTS `compRep`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `compRep` (
-  `compRep_id` int(11) NOT NULL AUTO_INCREMENT,
-  `comp_name` varchar(256) DEFAULT NULL,
-  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`compRep_id`),
-  CONSTRAINT `compRep_ibfk_1` FOREIGN KEY (`compRep_id`) REFERENCES `User` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `compRep`
---
-
-LOCK TABLES `compRep` WRITE;
-/*!40000 ALTER TABLE `compRep` DISABLE KEYS */;
-INSERT INTO `compRep` VALUES (20,'AITools','2020-05-20 12:31:31'),(22,'Askldna','2020-05-20 12:33:02');
-/*!40000 ALTER TABLE `compRep` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (20,'Askari Iqbal','ai@g.com','$5$rounds=535000$s7YCwiwEZWcjkM.R$7kQ.RSC0Siu/gwjLrwNzT6/u0nBsn0Z203ZZiU4yvz/','2020-05-20 12:16:27'),(22,'Abdullah Qutb','aasfkjasndai@g.com','$5$rounds=535000$6ToEchMwP2mgMO02$o1MrAdlEUa8rfmD/Lz7Ob4eTEX9VekHkcq.XaxHLLjA','2020-05-20 12:33:02'),(23,'asdasd','123','$5$rounds=535000$lZTzS8xp0ZBtLfrR$YS7Yi3Osb6KQfbhaVwqUm.szxMa2OjKUFmK75haUz.6','2020-05-20 13:42:25'),(24,'123','1234','1234','2020-05-20 13:43:29'),(25,'123','12345','$5$rounds=535000$2oTd92KRbW6wxMJX$AJx9OeoHQf/HyrhKOaXVwvHMtsDacNiwZQQx2c0dV1C','2020-05-20 15:01:23'),(26,'123','12','123','2020-05-20 18:21:27'),(27,'test','test','test','2020-05-20 18:34:00');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -315,4 +340,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-20 15:34:44
+-- Dump completed on 2020-05-21 14:46:42
